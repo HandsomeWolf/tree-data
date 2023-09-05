7dccbb54c640e504ad4f1786f4f5edd79b74127c
mux <55856773@qq.com>
2023-09-05 12:09:53 +0800

    fix: filter

--- src/basic/traverse/filter.ts
+++ src/basic/traverse/filter.ts
@@ -1,55 +1,127 @@
-// 树形数据过滤 (Filter tree data)
+import _ from "lodash";
+
 export function filterTree(
   tree: TreeNode[],
   options?: {
     include?: { [key: string]: any[] };
     exclude?: { [key: string]: any[] };
     childrenKey?: string;
+    idKey?: string;
     isDeleteEmptyChildren?: boolean;
   },
 ): TreeNode[] {
+  const idKey = options?.idKey || "id";
   const childrenKey = options?.childrenKey || "children";
   const isDeleteEmptyChildren = options?.isDeleteEmptyChildren || false;
 
-  function filter(node: TreeNode): TreeNode | null {
+  const newTree: TreeNode[] = _.cloneDeep(tree);
+  const queue: TreeNode[] = [...newTree];
+  const parents: { [key: string]: TreeNode[] } = {};
+
+  while (!_.isEmpty(queue)) {
+    const node = queue.shift() as TreeNode;
+
     if (options?.exclude) {
-      for (const key of Object.keys(options.exclude)) {
-        if (options.exclude[key].includes(node[key])) {
-          return null;
-        }
-      }
+      markNodesForDeletion(node, options.exclude, idKey, parents);
     }
 
     if (options?.include) {
-      for (const key of Object.keys(options.include)) {
-        if (!options.include[key].includes(node[key])) {
-          return null;
-        }
-      }
+      markNodesForDeletion(node, options.include, idKey, parents, true);
     }
 
     const children = node[childrenKey as keyof TreeNode] as TreeNode[];
     if (children) {
-      node[childrenKey as keyof TreeNode] = children
-        .map((child) => filter(child))
-        .filter((child: TreeNode | null): child is TreeNode => child !== null);
-      if (
-        isDeleteEmptyChildren &&
-        node[childrenKey as keyof TreeNode].length === 0
-      ) {
-        return null;
-      }
-
-      // 如果子节点数组为空，删除children属性 (If the children array is empty, delete the children property)
-      if (node[childrenKey as keyof TreeNode].length === 0) {
-        delete node[childrenKey as keyof TreeNode];
+      for (const child of children) {
+        if (!parents[child[idKey]]) {
+          parents[child[idKey]] = [];
+        }
+        parents[child[idKey]].push(node);
+        queue.push(child);
       }
     }
-
-    return node;
   }
 
-  return tree
-    .map((child) => filter(child))
-    .filter((node: TreeNode | null): node is TreeNode => node !== null);
+  for (const parentList of Object.values(parents)) {
+    for (const parent of parentList) {
+      if (parent && parent.toBeDeletedChildren) {
+        _.remove(parent[childrenKey], (child: TreeNode) =>
+          parent.toBeDeletedChildren.includes(child[idKey]),
+        );
+        delete parent.toBeDeletedChildren;
+      }
+    }
+  }
+
+  if (isDeleteEmptyChildren) {
+    for (const parentList of Object.values(parents)) {
+      for (const parent of parentList) {
+        if (parent && _.isEmpty(parent[childrenKey])) {
+          delete parent[childrenKey];
+        }
+      }
+    }
+  }
+
+  return _.remove(newTree, (node) => !parents[node[idKey]]);
+}
+
+function markNodesForDeletion(
+  node: TreeNode,
+  filter: { [key: string]: any[] },
+  idKey: string,
+  parents: { [key: string]: TreeNode[] },
+  isInclude?: boolean,
+) {
+  for (const key of Object.keys(filter)) {
+    const shouldDelete = isInclude
+      ? !filter[key].includes(node[key]) &&
+        areAllChildrenExcluded(node, filter[key], key)
+      : filter[key].includes(node[key]);
+    if (shouldDelete) {
+      const parentList = _.get(parents, node[idKey]);
+      if (parentList) {
+        for (const parent of parentList) {
+          if (!parent.toBeDeletedChildren) {
+            parent.toBeDeletedChildren = [];
+          }
+          parent.toBeDeletedChildren.push(node[idKey]);
+        }
+      }
+    } else if (isInclude) {
+      let parentList = _.get(parents, node[idKey]);
+      while (parentList && parentList.length > 0) {
+        for (const parent of parentList) {
+          _.remove(parent.toBeDeletedChildren, (id) => id === node[idKey]);
+        }
+        parentList = _.get(parents, parentList[0][idKey]);
+      }
+    }
+  }
+}
+
+function areAllChildrenExcluded(
+  node: TreeNode,
+  includeIds: any[],
+  key: string,
+): boolean {
+  const stack: TreeNode[] = [node];
+  while (stack.length > 0) {
+    const current = stack.pop() as TreeNode;
+    const children = current.children || [];
+    for (const child of children) {
+      if (includeIds.includes(child[key])) {
+        return false;
+      }
+      if (
+        child.children &&
+        child.children.some((grandChild: TreeNode) =>
+          includeIds.includes(grandChild[key]),
+        )
+      ) {
+        return false;
+      }
+      stack.push(child);
+    }
+  }
+  return true;
 }
--- tests/basic/traverse/filter.test.ts
+++ tests/basic/traverse/filter.test.ts
@@ -10,6 +10,342 @@ describe("filterTree", () => {
           parentId: null,
           children: [
             { id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] },
+            { id: 3, parentId: 1, children: [{ id: 4, parentId: 3 }] },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            { id: 2, parentId: 1, children: [] },
+            { id: 3, parentId: 1, children: [] },
+          ],
+        },
+      ];
+
+      expect(filterTree(tree, { exclude: { id: [4] } })).toEqual(expected);
+    });
+    it("repeat key", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [{ id: 4, parentId: 2, name: "wolf" }],
+            },
+            {
+              id: 3,
+              parentId: 1,
+              children: [{ id: 5, parentId: 3, name: "wolf" }],
+            },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            { id: 2, parentId: 1, children: [] },
+            { id: 3, parentId: 1, children: [] },
+          ],
+        },
+      ];
+
+      expect(filterTree(tree, { exclude: { name: ["wolf"] } })).toEqual(
+        expected,
+      );
+    });
+    it("many", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            { id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [{ id: 2, parentId: 1, children: [] }],
+        },
+      ];
+
+      expect(filterTree(tree, { exclude: { id: [4, 3] } })).toEqual(expected);
+    });
+
+    it("isDeleteEmptyChildren -> true", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    { id: 5, parentId: 4, children: [{ id: 6, parentId: 5 }] },
+                  ],
+                },
+              ],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                },
+              ],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+
+      expect(
+        filterTree(tree, {
+          exclude: { id: [5, 6] },
+          isDeleteEmptyChildren: true,
+        }),
+      ).toEqual(expected);
+    });
+
+    it("isDeleteEmptyChildren -> false", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    { id: 5, parentId: 4, children: [{ id: 6, parentId: 5 }] },
+                  ],
+                },
+              ],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+
+      expect(
+        filterTree(tree, {
+          exclude: { id: [4, 6] },
+          isDeleteEmptyChildren: false,
+        }),
+      ).toEqual(expected);
+    });
+  });
+
+  describe("include", () => {
+    it("normal", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        {
+                          id: 6,
+                          parentId: 5,
+                          children: [{ id: 7, parentId: 6 }],
+                        },
+                        {
+                          id: 8,
+                          parentId: 5,
+                          children: [{ id: 9, parentId: 8 }],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        {
+                          id: 6,
+                          parentId: 5,
+                          children: [],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ];
+
+      expect(filterTree(tree, { include: { id: [6] } })).toEqual(expected);
+    });
+    it("normal", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        {
+                          id: 6,
+                          parentId: 5,
+                          name: "wolf",
+                          children: [{ id: 7, parentId: 6 }],
+                        },
+                        {
+                          id: 6,
+                          parentId: 5,
+                          name: "wolf",
+                          children: [{ id: 7, parentId: 6 }],
+                        },
+                        {
+                          id: 8,
+                          parentId: 5,
+                          children: [{ id: 9, parentId: 8 }],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            { id: 3, parentId: 1 },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                {
+                  id: 4,
+                  parentId: 2,
+                  children: [
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        { id: 6, parentId: 5, name: "wolf", children: [] },
+                        { id: 6, parentId: 5, name: "wolf", children: [] },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ];
+      // expect(filterTree(tree, { include: { id: [6] } })).toEqual(expected);
+      expect(filterTree(tree, { include: { name: ["wolf"] } })).toEqual(
+        expected,
+      );
+    });
+    it("isDeleteEmptyChildren -> true", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            {
+              id: 2,
+              parentId: 1,
+              children: [
+                { id: 4, parentId: 2 },
+                { id: 5, parentId: 2 },
+              ],
+            },
             { id: 3, parentId: 1 },
           ],
         },
@@ -24,28 +360,20 @@ describe("filterTree", () => {
         },
       ];
 
-      expect(filterTree(tree, { exclude: { id: [3] } })).toEqual(expected);
+      expect(
+        filterTree(tree, {
+          include: { id: [4] },
+          isDeleteEmptyChildren: true,
+        }),
+      ).toEqual(expected);
     });
-
-    it("deep", () => {
+    it("isDeleteEmptyChildren -> false", () => {
       const tree = [
         {
           id: 1,
           parentId: null,
           children: [
-            {
-              id: 2,
-              parentId: 1,
-              children: [
-                {
-                  id: 4,
-                  parentId: 2,
-                  children: [
-                    { id: 5, parentId: 4, children: [{ id: 6, parentId: 5 }] },
-                  ],
-                },
-              ],
-            },
+            { id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] },
             { id: 3, parentId: 1 },
           ],
         },
@@ -54,29 +382,18 @@ describe("filterTree", () => {
         {
           id: 1,
           parentId: null,
-          children: [
-            {
-              id: 2,
-              parentId: 1,
-              children: [
-                {
-                  id: 4,
-                  parentId: 2,
-                  children: [{ id: 5, parentId: 4 }],
-                },
-              ],
-            },
-            { id: 3, parentId: 1 },
-          ],
+          children: [{ id: 3, parentId: 1 }],
         },
       ];
 
       expect(
-        filterTree(tree, { exclude: { id: [6] }, isDeleteEmptyChildren: true }),
+        filterTree(tree, {
+          include: { id: [3] },
+          isDeleteEmptyChildren: false,
+        }),
       ).toEqual(expected);
     });
-
-    it("should exclude specified nodes", () => {
+    it("many", () => {
       const tree = [
         {
           id: 1,
@@ -90,7 +407,22 @@ describe("filterTree", () => {
                   id: 4,
                   parentId: 2,
                   children: [
-                    { id: 5, parentId: 4, children: [{ id: 6, parentId: 5 }] },
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        {
+                          id: 6,
+                          parentId: 5,
+                          children: [{ id: 7, parentId: 6 }],
+                        },
+                        {
+                          id: 8,
+                          parentId: 5,
+                          children: [{ id: 9, parentId: 8 }],
+                        },
+                      ],
+                    },
                   ],
                 },
               ],
@@ -111,7 +443,16 @@ describe("filterTree", () => {
                 {
                   id: 4,
                   parentId: 2,
-                  children: [{ id: 5, parentId: 4 }],
+                  children: [
+                    {
+                      id: 5,
+                      parentId: 4,
+                      children: [
+                        { id: 6, parentId: 5 },
+                        { id: 8, parentId: 5 },
+                      ],
+                    },
+                  ],
                 },
               ],
             },
@@ -122,31 +463,38 @@ describe("filterTree", () => {
 
       expect(
         filterTree(tree, {
-          exclude: { id: [6] },
-          isDeleteEmptyChildren: false,
+          include: { id: [6, 8, 3] },
+          isDeleteEmptyChildren: true,
         }),
       ).toEqual(expected);
     });
   });
 
-  it("should include specified nodes", () => {
-    const tree = [
-      {
-        id: 1,
-        parentId: null,
-        children: [
-          { id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] },
-          { id: 3, parentId: 1 },
-        ],
-      },
-    ];
-    const expected = [
-      {
-        id: 1,
-        parentId: null,
-        children: [{ id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] }],
-      },
-    ];
-    expect(filterTree(tree, { exclude: { id: [3] } })).toEqual(expected);
+  describe("exclude and include", () => {
+    it("normal", () => {
+      const tree = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            { id: 2, parentId: 1, children: [{ id: 4, parentId: 2 }] },
+            { id: 3, parentId: 1, children: [{ id: 4, parentId: 3 }] },
+          ],
+        },
+      ];
+      const expected = [
+        {
+          id: 1,
+          parentId: null,
+          children: [
+            { id: 3, parentId: 1, children: [{ id: 4, parentId: 3 }] },
+          ],
+        },
+      ];
+
+      expect(
+        filterTree(tree, { exclude: { id: [4] }, include: { parentId: [3] } }),
+      ).toEqual(expected);
+    });
   });
 });
